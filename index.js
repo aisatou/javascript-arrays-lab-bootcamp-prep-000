44bc21de8149b2c39a4dc5c8fa50faa9be05c4df
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-17 19:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newKittens = [kittens, ...name]
+  var newKittens = [name, ...kittens]
   return newKittens
 }
