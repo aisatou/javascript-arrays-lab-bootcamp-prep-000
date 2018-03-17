e4045706080b1605fea56e2f94f629d347a1b484
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-17 20:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newArray = [kittens.slice];
+  var newArray = kittens.slice;
   return newArray.slice(0, newArray.length - 1);
 }
